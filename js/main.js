d1b700b2a4863761fdf50f911a218bf500335f2a
amarnuhanovic23 <amarmanji1234@gmail.com>
2026-06-09 20:45:35 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('#sort').sortable();
+
+$(function() {
+    $('#accordion').accordion();
+});
+
+$('#tabs').tabs();
+
+// $(function() {
+//    $('#accordion').accordion({
+//        collapsible: true,
+//        });
+//    
+//});
+
+// $(function() {
+//    $('#tabs').tabs({
+//        collapsible: true,
+//        });
+//    
+//});
